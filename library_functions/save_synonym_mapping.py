8bdc4c771eda8a53c7fc069e2b3a831c6c4a6f28
Luca Dorigo <dorigoluca@gmail.com>
2020-11-24 10:40:39 +0100

    Improve functions and notebook to save synonyms/names

--- library_functions/save_synonym_mapping.py
+++ library_functions/save_synonym_mapping.py
@@ -3,7 +3,13 @@ from config import Config
 
 
 def save_synonym_mapping(wiki_data):
-    # Small snippet to save an inverse mapping from synonyms to names
+    """Save a mapping from synonyms to substance names
+
+    Args:
+        wiki_data (Dict): Flat dict of substance names
+    """
+    
+        # Small snippet to save an inverse mapping from synonyms to names
     synonym_mapping = {}
 
     for index, name in enumerate(wiki_data["name"]):
@@ -14,3 +20,12 @@ def save_synonym_mapping(wiki_data):
 
     with open(Config.Path.synonym_mapping, "w+") as f:
         json.dump(synonym_mapping, f, indent=2)
+
+def save_substance_names(wiki_data):
+    """Save the list of all substance names to a json file.
+
+    Args:
+        wiki_data (Dict): Dict containing the wikipedia data (flat)
+    """
+    with open(Config.Path.substance_names, "w+") as f:
+        json.dump(wiki_data["name"], f, indent=2)
