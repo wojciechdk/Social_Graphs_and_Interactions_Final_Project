e9e33ea2a2221ef9eeb6f4bbe55b4178cb924d8a
Luca Dorigo <dorigoluca@gmail.com>
2020-11-24 16:07:53 +0100

    Add nice node hover text

--- library_functions/plotly_draw.py
+++ library_functions/plotly_draw.py
@@ -1,15 +1,19 @@
 #%%
 
-from typing import Dict, Tuple, Union
+from typing import Dict, List, Tuple, Union
 from networkx.drawing import layout
+from networkx.generators import line
 import plotly.graph_objects as go
 import networkx as nx
-
+import json
 from config import Config
 
 G = nx.random_geometric_graph(200, 0.125)
 # %%
-
+with open(Config.Path.contents_per_substance, "r+") as f:
+    contents_per_substance =json.load(f)
+with open(Config.Path.urls_per_substance, "r+") as f:
+    url_per_substance =json.load(f)
 
 def get_edge_trace(
     graph: nx.Graph,
@@ -36,10 +40,45 @@ def get_edge_trace(
     return edge_trace
 
 
+def split_text_at(text :str, length: int) -> str :
+    words = text.split()
+
+    results = []
+    line_length = 0
+    for word in words:
+        if line_length < length:
+            results.append(word)
+            line_length += len(word)
+        else: 
+            results.append("<br>")
+            results.append(word)
+            line_length = len(word)
+
+    return " ".join(results )
+
+    
+def get_nodes_hover(graph: nx.Graph) -> List[str]:
+    node_hovers = []
+
+    for name , data in graph.nodes(data=True):
+        hover = f"Nootropic: {name}, Degree: {graph.degree(name)}. <br>"
+
+        if "categories" in data:
+            hover += f"\n Part of {len(data['categories'])} categories. <br><br>"
+
+
+        hover += f"\"{split_text_at(contents_per_substance[name][:120], 40)}...\"<br> <br> <br> "
+
+        hover += f"Read more: {url_per_substance[name]}"
+        node_hovers.append(hover)
+    return node_hovers
+
+
 def get_nodes_trace(
     graph: nx.Graph,
     node_weight_attribute: str = None,
     node_color_attribute: str = None,
+    node_size_attribute: str = None,
     positions: Dict[Union[str, int], Tuple[int, int]] = None,
 ):
     nodes_x = []
@@ -48,9 +87,11 @@ def get_nodes_trace(
         nodes_x.append(positions[node][0])
         nodes_y.append(positions[node][1])
 
+    hovers = get_nodes_hover(graph)
+
     node_names = list(graph.nodes)
     node_trace = go.Scatter(
-        x=nodes_x, y=nodes_y, text=node_names, mode="markers", hoverinfo="text"
+        x=nodes_x, y=nodes_y, text=hovers, mode="markers", hoverinfo="text"
     )
 
     if node_color_attribute:
@@ -80,6 +121,12 @@ def draw_graph_plotly(
         data=data,
         layout=go.Layout(
             showlegend=False,
+            margin={
+                "l": 20,
+                "r": 20,
+                "t": 25,
+                "b": 25
+            },
             xaxis={"showgrid": False, "zeroline": False, "showticklabels": False},
             yaxis={"showgrid": False, "zeroline": False, "showticklabels": False},
         ),
@@ -87,6 +134,3 @@ def draw_graph_plotly(
     return figure
 
 
-# %%
-draw_graph_plotly(G)
-# %%
