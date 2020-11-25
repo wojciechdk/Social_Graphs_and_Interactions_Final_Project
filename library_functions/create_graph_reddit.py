afc5fec5ec7b1867173e1fd85acbd8b336539bc2
Wojciech Mazurkiewicz <wojciechdk@gmail.com>
2020-11-25 13:37:17 +0100

    Network comparison with plots of distribution of attributes

--- library_functions/create_graph_reddit.py
+++ library_functions/create_graph_reddit.py
@@ -3,17 +3,22 @@ import networkx as nx
 
 
 def create_graph_reddit():
+    # Load the clean Reddit and Wiki data
     drug_database_reddit = lf.load_data_reddit()
     wiki_data = lf.load_data_wiki()
     substance_names = lf.load_substance_names()
 
+    # Initialize graphs
     g_reddit = nx.Graph()
     g_reddit.add_nodes_from(substance_names)
 
-    # Assign categories from Wikipedia to drugs
+    # Initialize mention counts and assign categories from Wikipedia
+    # to nodes (drugs)
     for index_drug, drug in enumerate(wiki_data['name']):
         if drug in g_reddit.nodes:
             g_reddit.nodes[drug]['count'] = 0
+            g_reddit.nodes[drug]['polarity'] = []
+            g_reddit.nodes[drug]['subjectivity'] = []
             g_reddit.nodes[drug]['categories'] = \
                 wiki_data['categories'][index_drug]
 
@@ -31,12 +36,16 @@ def link_drugs(G: nx.Graph, list_of_drugs, polarity, subjectivity):
     if len(list_of_drugs) <= 1:
         return G
     else:
+        # Increase the count of the drug mentions
+        for drug in list_of_drugs:
+            if (drug in G.nodes):
+                G.nodes[drug]['count'] += 1
+                G.nodes[drug]['polarity'].append(polarity)
+                G.nodes[drug]['subjectivity'].append(subjectivity)
+
         for index in range(len(list_of_drugs)):
             drug = list_of_drugs[index]
             other_drugs = list_of_drugs[(index + 1):]
-
-            # Increase the count
-            G.nodes[drug]['count'] += 0
 
             for other_drug in other_drugs:
                 if (drug in G.nodes) & (other_drug in G.nodes):
--- notebooks/network_analysis/network_comparison.py
+++ notebooks/network_analysis/network_comparison.py
@@ -17,7 +17,20 @@ from pathlib import Path
 g_wiki = lf.create_graph_wiki()
 g_reddit = lf.create_graph_reddit()
 
-#%% Plot degree distribution summary
+#%%
+# reddit_data = lf.load_data_reddit()
+#
+# max_matches = 0
+# label_max = ''
+# for post_label, post in reddit_data.items():
+#     if len(post['matches']) > max_matches:
+#         max_matches = len(post['matches'])
+#         label_max = post_label
+#
+# post_with_most_matches = reddit_data[label_max]
+
+
+# %% Plot degree distribution summary
 graphs_to_show = [g_reddit, g_wiki, w.graph.erdos_renyi_like(g_reddit)]
 graph_names = ['Reddit', 'Wiki', 'Random like Reddit']
 graph_colors = ['red', 'blue', 'green']
@@ -29,16 +42,14 @@ w.graph.plot_degree_distribution_summary(graphs_to_show,
                                          x_lim_log=(0.9, 1000)
                                          )
 
-
 # %% Most central nodes Wiki
 for centrality in ['degree', 'in-degree', 'out-degree', 'betweenness',
                    'eigenvector']:
     w.graph.most_central_nodes(g_wiki, centrality, n=10, printout=True)
 
-#%% Most central nodes Reddit
+# %% Most central nodes Reddit
 for centrality in ['degree', 'betweenness', 'eigenvector']:
     w.graph.most_central_nodes(g_reddit, centrality, n=10, printout=True)
-
 
 # %% In vs out degree Wiki
 w.graph.plot_in_vs_out_degree(g_wiki,
@@ -46,7 +57,7 @@ w.graph.plot_in_vs_out_degree(g_wiki,
                               colormap_norm='log')
 plt.show()
 
-#%% Force Atlas plot Wiki
+# %% Force Atlas plot Wiki
 # w.graph.plot_force_atlas_2(w.graph.remove_isolates(g_wiki),
 #                            node_size='by degree',
 #                            node_color=np.array([[0, 0, 1, 0.5]]),
@@ -56,30 +67,47 @@ plt.show()
 #                            outboundAttractionDistribution=True)
 # plt.show()
 
-#%% Find most often occurring edges
+# %% Find most often occurring edges
 # edges_count = nx.get_edge_attributes(g_wiki, "count")
 # edges_count_sorted = sorted(edges_count.items(),
 #                             key=itemgetter(1),
 #                             reverse=True)
 
 
-#%% Create alternative reddit graphs
+# %% Create alternative reddit graphs
 # Positive
 g_reddit_positive = g_reddit.copy()
-conditions_not_positive = {'polarity': lambda x: x < 0.2}
+conditions_not_positive = {'polarity': lambda x: np.median(x) < 0.2}
 edges_not_positive = w.graph.get_edges_by_attribute(g_reddit_positive,
                                                     conditions_not_positive)
 g_reddit_positive.remove_edges_from(edges_not_positive)
 
 # Negative
 g_reddit_negative = g_reddit.copy()
-conditions_not_negative = {'polarity': lambda x: x > -0.2}
+conditions_not_negative = {'polarity': lambda x: np.median(x) > -0.2}
 edges_not_negative = w.graph.get_edges_by_attribute(g_reddit_negative,
                                                     conditions_not_negative)
 g_reddit_negative.remove_edges_from(edges_not_negative)
 
+# %% Plot distribution of attributes
+figure = plt.figure(figsize=(12, 8))
+axes = figure.gca()
 
-# Evaluate the positive reddit
+w.graph.plot_distribution(
+    g_reddit,
+    quantity='attribute',
+    attribute_object='edge',
+    attribute_name='polarity',
+    attribute_function=np.median,
+    attribute_function_name='median',
+    plot_type='bar',
+    axes=axes,
+    bins=100
+)
+
+plt.show()
+
+# %% Degree distribution of Reddit plots
 graphs_to_show = [g_reddit, g_reddit_positive, g_reddit_negative]
 graph_names = ['Reddit', 'Reddit positive', 'Reddit negative']
 graph_colors = ['red', 'green', 'blue']
@@ -90,20 +118,18 @@ w.graph.plot_degree_distribution_summary(graphs_to_show,
                                          x_lim_lin=(-2, 100),
                                          x_lim_log=(0.9, 1000)
                                          )
-#%% Degree stats
-print('\nReddit:')
-degree_stats_reddit = w.graph.degree_statistics(g_reddit, printout=True)
-
-print('\nReddit Positive:')
-degree_stats_g_reddit_positive = w.graph.degree_statistics(g_reddit_positive, printout=True)
-
-print('\nReddit Negative:')
-degree_stats_g_reddit_negative = w.graph.degree_statistics(g_reddit_negative, printout=True)
 
 
-#%% Most central nodes in different Reddit Graphs
-print('Reddit')
+# %% Degree stats
+graphs = [g_reddit, g_reddit_positive, g_reddit_negative]
+graph_names = ['Reddit', 'Reddit Positive', 'Reddit Negative']
 
+degree_stats = dict()
+for graph, graph_name in zip(graphs, graph_names):
+    print(f'\n{graph_name}:')
+    degree_stats[graph_name] = w.graph.degree_statistics(graph, printout=True)
+
+# %% Most central nodes in different Reddit Graphs
 graphs = [g_reddit, g_reddit_positive, g_reddit_negative]
 graph_names = ['Reddit', 'Reddit Positive', 'Reddit Negative']
 centralities = ['degree']
@@ -120,8 +146,8 @@ nx.readwrite.gpickle.write_gpickle(g_wiki, Config.Path.wiki_digraph)
 
 gcc_wiki_dir = w.graph.largest_connected_component(g_wiki)
 gcc_reddit = w.graph.largest_connected_component(g_reddit)
-# %%
 
+# %%
 gcc_wiki = gcc_wiki_dir.to_undirected()
 
 nx.readwrite.gpickle.write_gpickle(gcc_reddit, Config.Path.reddit_gcc)
