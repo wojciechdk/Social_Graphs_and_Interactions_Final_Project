58cd3c55f00ec6c25080b30f8b307724a7a85003
Luca Dorigo <dorigoluca@gmail.com>
2020-11-27 10:48:56 +0100

    Allow to specify alternative in/out paths

--- library_functions/calculate_sentiment_reddit.py
+++ library_functions/calculate_sentiment_reddit.py
@@ -5,14 +5,22 @@ from textblob import TextBlob
 from tqdm import tqdm
 
 
-def calculate_sentiment_reddit():
-    with open(Config.Path.reddit_data_with_NER) as file:
+def calculate_sentiment_reddit(alternative_path=None, alternative_path_out=None):
+    filepath_in = (
+        alternative_path if alternative_path else Config.Path.reddit_data_with_NER
+    )
+    filepath_out = (
+        alternative_path_out
+        if alternative_path_out
+        else Config.Path.reddit_data_with_NER_and_sentiment
+    )
+    with open(filepath_in) as file:
         drug_database_reddit = json.load(file)
 
     for post_id, post in tqdm(drug_database_reddit.items()):
-        analysis = TextBlob(post['content'])
-        post['polarity'] = analysis.polarity
-        post['subjectivity'] = analysis.subjectivity
+        analysis = TextBlob(post["content"])
+        post["polarity"] = analysis.polarity
+        post["subjectivity"] = analysis.subjectivity
 
-    with open(Config.Path.reddit_data_with_NER_and_sentiment, 'w+') as file:
+    with open(filepath_out, "w+") as file:
         json.dump(drug_database_reddit, file)
