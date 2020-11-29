59eab3e2303a791f461e53f7801c1b416baa9a7e
Luca Dorigo <dorigoluca@gmail.com>
2020-11-29 12:08:40 +0100

    add functions to assign the wikipedia root community to a graph's nodes,
    and to compute multiple levels of communities with the louvain
    algorithm

--- library_functions/communities.py
+++ library_functions/communities.py
@@ -1,6 +1,6 @@
 #%%
 
-from typing import Optional, Tuple, Union
+from typing import Dict, List, Optional, Tuple, Union
 import community
 import networkx as nx
 from infomap import Infomap
@@ -21,35 +21,53 @@ def assign_louvain_communities(
     Returns:
         Union[nx.Graph, Tuple[nx.Graph, nx.Graph]]: [description]
     """
-    reddit_partition = community.best_partition(reddit_graph, weight=reddit_edge_weight)
+    reddit_dendrogram = community.generate_dendrogram(
+        reddit_graph, weight=reddit_edge_weight
+    )
     if wiki_graph:
-        wiki_partition = community.best_partition(wiki_graph)
+        wiki_dendrogram = community.generate_dendrogram(wiki_graph)
     for node in reddit_graph:
-        reddit_graph.nodes[node]["louvain_community_reddit"] = reddit_partition[node]
+        reddit_graph.nodes[node][f"louvain_community_reddit"] = {}
+        for level in range(len(reddit_dendrogram) - 1):
+            partition = community.partition_at_level(reddit_dendrogram, level)
+            reddit_graph.nodes[node][f"louvain_community_reddit"][level] = partition[
+                node
+            ]
         if wiki_graph:
             # Also add the community from the other graph to allow comparing
+            reddit_graph.nodes[node][f"louvain_community_wiki"] = {}
             try:
-                reddit_graph.nodes[node]["louvain_community_wiki"] = wiki_partition[
-                    node
-                ]
+                for level in range(len(wiki_dendrogram) - 1):
+                    partition = community.partition_at_level(wiki_dendrogram, level)
+                    reddit_graph.nodes[node][f"louvain_community_wiki"][
+                        level
+                    ] = partition[node]
             except:
                 reddit_graph.nodes[node]["louvain_community_wiki"] = -1
     if wiki_graph:  # %%
         for node in wiki_graph:
-            wiki_graph.nodes[node]["louvain_community_wiki"] = wiki_partition[node]
-
-            # Also add the community from the other graph to allow comparing
-            try:
-                wiki_graph.nodes[node]["louvain_community_reddit"] = reddit_partition[
+            wiki_graph.nodes[node][f"louvain_community_wiki"] = {}
+            for level in range(len(wiki_dendrogram) - 1):
+                partition = community.partition_at_level(wiki_dendrogram, level)
+                wiki_graph.nodes[node][f"louvain_community_wiki"][level] = partition[
                     node
                 ]
+            # Also add the community from the other graph to allow comparing
+
+            wiki_graph.nodes[node][f"louvain_community_reddit"] = {}
+            try:
+                for level in range(len(reddit_dendrogram) - 1):
+                    partition = community.partition_at_level(reddit_dendrogram, level)
+                    wiki_graph.nodes[node][f"louvain_community_reddit"][
+                        level
+                    ] = partition[node]
             except:
                 wiki_graph.nodes[node]["louvain_community_reddit"] = -1
 
     return (
-        (reddit_graph, reddit_partition, wiki_graph, wiki_partition)
+        (reddit_graph, reddit_dendrogram, wiki_graph, wiki_dendrogram)
         if wiki_graph
-        else (reddit_graph, reddit_partition)
+        else (reddit_graph, reddit_dendrogram)
     )
 
 
@@ -82,6 +100,35 @@ def get_infomap_communities(graph: nx.Graph, reddit_edge_weight=None):
             ] = node.module_id
 
     return graph
+
+
+def assign_root_categories(
+    graph: nx.Graph,
+    wiki_data: Dict[str, List],
+    mapping: Dict[str, List[str]],
+    name: str,
+):
+    """Given a graph and wikipedia data, assign a new attribute to the nodes that represent the root category of that node on wikipedia.
+
+
+    Args:
+        graph (nx.Graph): nootropics graph
+        wiki_data (Dict[str, List]): wikipedia data as obtained by lf.load_wiki_data
+        mapping (Dict[str, List[str]]): Dict of the form "root_category":["list","of","sub","categories"]
+        name (str): name of the new node attribute to which to assign the mapping.
+    """
+    inverse_mapping = {}
+    for category, subcategories in mapping.items():
+        for subcategory in subcategories:
+            inverse_mapping[subcategory] = category
+
+    names_to_categories = dict(zip(wiki_data["name"], wiki_data["categories"]))
+    for node in graph.nodes:
+        graph.nodes[node][name] = []
+
+        for category in names_to_categories[node]:
+            if category in inverse_mapping:
+                graph.nodes[node][name].append(inverse_mapping[category])
 
 
 # %%
