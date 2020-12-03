a07ab8f1d5d255ff035492b7b2c0336f2d1c4f10
Luca Dorigo <dorigoluca@gmail.com>
2020-12-03 12:57:57 +0100

    Adapt function to allow loading layouts from disk

--- library_functions/layouting.py
+++ library_functions/layouting.py
@@ -1,4 +1,5 @@
 # %% Functions to place nodes on a 2d space
+import json
 import networkx as nx
 from fa2 import ForceAtlas2
 
@@ -6,6 +7,10 @@ try:
     import library_functions as lf
 except ModuleNotFoundError:
     import project.library_functions as lf
+try:
+    from config import Config
+except ModuleNotFoundError:
+    from project.config import Config
 
 # %%
 
@@ -30,7 +35,35 @@ forceatlas2 = ForceAtlas2(
 )
 
 
-def get_fa2_layout(graph: nx.Graph, edge_weight_attribute: str = None):
+def get_fa2_layout(
+    graph: nx.Graph,
+    edge_weight_attribute: str = None,
+    saved: str = None,
+    save: str = None,
+):
+    """Generate a layout using the fa2 algorithm, or load one from disk.
+
+    Args:
+        graph (nx.Graph): graph for which to compute the layout
+        edge_weight_attribute (str, optional): Which edge attribute to use for computing the layout. WARNING: only works with latest fa2 version.
+        saved (str, optional): If not none, load the layout from the given path instead of computing it. Defaults to None.
+        save (str, optional): if not none, save the layout to the given path. Defaults to None.
+
+    Returns:
+        Dict: networkx layout giving the position of nodes
+    """
+
+    if saved:
+        with open(Config.Path.shared_data_folder / saved, "r") as f:
+            layout = json.load(f)
+            assert (
+                len(layout) == graph.number_of_nodes()
+            ), "Loaded layout does not have the same amount of nodes as the given graph"
+            for node in graph.nodes:
+                assert (
+                    node in layout
+                ), "The graph contains a node that was not found in the layout"
+            return layout
     try:
         if edge_weight_attribute:
             layout = forceatlas2.forceatlas2_networkx_layout(
@@ -46,6 +79,10 @@ def get_fa2_layout(graph: nx.Graph, edge_weight_attribute: str = None):
         )
         print("See https://github.com/bhargavchippada/forceatlas2 ")
         raise
+
+    if save:
+        with open(Config.Path.shared_data_folder / save, "w") as f:
+            json.dump(f, layout)
     return layout
 
 
