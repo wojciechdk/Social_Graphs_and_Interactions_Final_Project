50aababef1ce4da12d7805a5ca49b66ecdf8f7ab
Wojciech Mazurkiewicz <wojciechdk@gmail.com>
2020-12-03 12:24:46 +0100

    Reddit graph: changed attribute name "contents" to "content" to match Wiki-graph

--- library_functions/create_graph_reddit.py
+++ library_functions/create_graph_reddit.py
@@ -119,23 +119,24 @@ def link_drugs(
             G.nodes[drug]["polarity"].append(polarity)
             G.nodes[drug]["subjectivity"].append(subjectivity)
             if include_node_contents:
-                G.nodes[drug]["contents"].append(text)
+                G.nodes[drug]["content"].append(text)
 
     # Assign the edge attributes.
     for index in range(len(list_of_drugs)):
         drug = list_of_drugs[index]
-        other_drugs = list_of_drugs[(index + 1) :]
+        other_drugs = list_of_drugs[(index + 1):]
 
         for other_drug in other_drugs:
             if (drug in G.nodes) & (other_drug in G.nodes):
                 edge = (drug, other_drug)
                 if G.has_edge(*edge):
                     G.edges[edge]["count"] += 1
-                    G.edges[edge]["number_of_drugs_in_post"].append(len(list_of_drugs))
+                    G.edges[edge]["number_of_drugs_in_post"].append(
+                        len(list_of_drugs))
                     G.edges[edge]["polarity"].append(polarity)
                     G.edges[edge]["subjectivity"].append(subjectivity)
                     if include_link_contents:
-                        G.edges[edge]["contents"].append(text)
+                        G.edges[edge]["content"].append(text)
                 else:
                     G.add_edge(
                         *edge,
@@ -149,7 +150,8 @@ def link_drugs(
 
 def weigh_attribute(attribute_to_weigh, all_edge_attributes):
     value_attribute = np.array(all_edge_attributes[attribute_to_weigh])
-    number_of_drugs_in_post = np.array(all_edge_attributes["number_of_drugs_in_post"])
+    number_of_drugs_in_post =\
+        np.array(all_edge_attributes["number_of_drugs_in_post"])
     weights = 1 / (number_of_drugs_in_post - 1)
 
     number_of_posts_containing_link = all_edge_attributes["count"]
