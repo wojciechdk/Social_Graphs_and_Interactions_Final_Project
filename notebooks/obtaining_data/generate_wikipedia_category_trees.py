cb2771c2acd6f1bd1831f2446024543dfeb09196
Luca Dorigo <dorigoluca@gmail.com>
2020-12-07 11:34:51 +0100

    Clean up notebooks for obtaining data, and add explanations in the explainer notebook

--- notebooks/obtaining_data/generate_wikipedia_category_trees.py
+++ notebooks/obtaining_data/generate_wikipedia_category_trees.py
@@ -13,109 +13,109 @@ import json
 mw = MediaWiki()
 
 
+%% [markdown]
+Obtain a "category tree" for psychotropics from which we weed out irrelevant stuff:
+
+%%
+ct= mw.categorytree("Drugs by psychological effects", depth=15)
+
 # %% [markdown]
-# Obtain a "category tree" for psychotropics from which we weed out irrelevant stuff:
+# This takes a long time to compute, so let's save it to json in case we need to recover it:
 
 # %%
-# ct= mw.categorytree("Drugs by psychological effects", depth=15)
+with open("psychoactive_category_tree_raw.json", "w+") as f:
+    json.dump(ct, f)
+#%% 
 
-# # %% [markdown]
-# # This takes a long time to compute, so let's save it to json in case we need to recover it:
+with open("psychoactive_category_tree_raw.json", "r") as f:
+    ct = json.load(f)
 
-# # %%
-# with open("psychoactive_category_tree_raw.json", "w+") as f:
-#     json.dump(ct, f)
-# #%% 
+# %%
+root_tree = ct["Drugs by psychological effects"]
+root_subs = root_tree["sub-categories"]
+psychoanaleptics_subs = root_subs["Psychoanaleptics"]["sub-categories"]
+stimulants_tree = psychoanaleptics_subs["Stimulants"]
 
-# with open("psychoactive_category_tree_raw.json", "r") as f:
-#     ct = json.load(f)
+# %% [markdown]
+# Do some manual cleaning to remove noise:
+# %% [markdown]
+# ### Stimulants
 
-# # %%
-# root_tree = ct["Drugs by psychological effects"]
-# root_subs = root_tree["sub-categories"]
-# psychoanaleptics_subs = root_subs["Psychoanaleptics"]["sub-categories"]
-# stimulants_tree = psychoanaleptics_subs["Stimulants"]
+# %%
+h_f_stimulants = stimulants_tree["sub-categories"]['Herbal and fungal stimulants']
 
-# # %% [markdown]
-# # Do some manual cleaning to remove noise:
-# # %% [markdown]
-# # ### Stimulants
+tobacco = h_f_stimulants["sub-categories"]["Tobacco"]
+tobacco_sub = tobacco["sub-categories"]
+tobacco_links = tobacco["links"]
 
-# # %%
-# h_f_stimulants = stimulants_tree["sub-categories"]['Herbal and fungal stimulants']
-
-# tobacco = h_f_stimulants["sub-categories"]["Tobacco"]
-# tobacco_sub = tobacco["sub-categories"]
-# tobacco_links = tobacco["links"]
-
-# # Other than "snus", subcategories are irrelevant
-# to_delete = []
-# for i in tobacco_sub.keys():
-#     if i not in ["Snus"]:
-#         print(f"Deleted {i}")
-#         to_delete.append(i)
+# Other than "snus", subcategories are irrelevant
+to_delete = []
+for i in tobacco_sub.keys():
+    if i not in ["Snus"]:
+        print(f"Deleted {i}")
+        to_delete.append(i)
         
-# for td in to_delete:
-#     try:
-#         del tobacco_sub[td]
-#     except:
-#         print(f"could not delete {td}")
-# # Delete irrelevant pages:
+for td in to_delete:
+    try:
+        del tobacco_sub[td]
+    except:
+        print(f"could not delete {td}")
+# Delete irrelevant pages:
 
-# for i in ["List of tobacco-related topics",
-#           "Animals and tobacco smoke", 
-#           "Cultivation of tobacco",
-#           "Curing of tobacco",   
-#           "George Augustine Washington",
-#             "Types of tobacco",
-#             "Tobacco smoke",
-#             "Tobacco products",
-#             "Tobacco pouch",
-#             "Tobacco politics",
-#             "Ten Motives",
-#             "Tobacco and art",
-#             "Tobacco and other drugs",
-#             "Tobacco BY-2 cells",
-#             "Tobacco factory",
-#          "Prevalence of tobacco use"]:
-#     try:
-#         print(f"removing {i}")
-#         tobacco_links.remove(i)
-#     except:
-#         print(f"Could not remove {i}")
-# h_f_stimulants['links'].remove("Areca nut production in India")
-# stimulant_subcategories = stimulants_tree["sub-categories"]
-# sm_amines=stimulant_subcategories['Sympathomimetic amines']
-# sm_amines['links'].remove("List of methylphenidate analogues")
-# stimulants_tree["links"].remove("List of investigational sleep drugs")
-# stimulants_tree["links"].remove("List of phenyltropanes")
+for i in ["List of tobacco-related topics",
+          "Animals and tobacco smoke", 
+          "Cultivation of tobacco",
+          "Curing of tobacco",   
+          "George Augustine Washington",
+            "Types of tobacco",
+            "Tobacco smoke",
+            "Tobacco products",
+            "Tobacco pouch",
+            "Tobacco politics",
+            "Ten Motives",
+            "Tobacco and art",
+            "Tobacco and other drugs",
+            "Tobacco BY-2 cells",
+            "Tobacco factory",
+         "Prevalence of tobacco use"]:
+    try:
+        print(f"removing {i}")
+        tobacco_links.remove(i)
+    except:
+        print(f"Could not remove {i}")
+h_f_stimulants['links'].remove("Areca nut production in India")
+stimulant_subcategories = stimulants_tree["sub-categories"]
+sm_amines=stimulant_subcategories['Sympathomimetic amines']
+sm_amines['links'].remove("List of methylphenidate analogues")
+stimulants_tree["links"].remove("List of investigational sleep drugs")
+stimulants_tree["links"].remove("List of phenyltropanes")
 
-# # Delete the top-level stimulants category, which is duplicate
-# del root_subs["Stimulants"]
+# Delete the top-level stimulants category, which is duplicate
+del root_subs["Stimulants"]
 
-# # %% [markdown]
-# # ### nootropics
+# %% [markdown]
+# ### nootropics
 
-# # %%
-# nootropics_tree = psychoanaleptics_subs["Nootropics"]
-# nootropics_tree["links"].remove("Nootropic")
-# nootropics_tree["links"].remove("Neuroenhancement")
-# nootropics_tree["links"].remove("Performance-enhancing substance")
-# nootropics_tree["links"].remove("Template:Psychostimulants, agents used for ADHD and nootropics")
-# nootropics_subcategories = nootropics_tree["sub-categories"]
-# del nootropics_subcategories["Smart drugs in fiction"]
+# %%
+nootropics_tree = psychoanaleptics_subs["Nootropics"]
+nootropics_tree["links"].remove("Nootropic")
+nootropics_tree["links"].remove("Neuroenhancement")
+nootropics_tree["links"].remove("Performance-enhancing substance")
+nootropics_tree["links"].remove("Template:Psychostimulants, agents used for ADHD and nootropics")
+nootropics_subcategories = nootropics_tree["sub-categories"]
+del nootropics_subcategories["Smart drugs in fiction"]
 
-# # %% [markdown]
-# # ### Antidepressants
+# %% [markdown]
+# ### Antidepressants
 
-# ad_tree = psychoanaleptics_subs["Antidepressants"]
-# to_remove = ["Antidepressants","Template:Antidepressants","List of antidepressants",
-#      "List of countries by antidepressant consumption","Antidepressant discontinuation syndrome",
-#     "Antidepressants and suicide risk","Antidepressants in Japan",
-#     "List of investigational antidepressants","Listening to Prozac",
-#     "Neurobiological effects of physical exercise","Pharmacology of antidepressants",
-#     "Second-generation antidepressant","Serotonin antagonist and reuptake inhibitor",
-#      "Serotonin modulator and stimulator"]
+ad_tree = psychoanaleptics_subs["Antidepressants"]
+to_remove = ["Antidepressants","Template:Antidepressants","List of antidepressants",
+     "List of countries by antidepressant consumption","Antidepressant discontinuation syndrome",
+    "Antidepressants and suicide risk","Antidepressants in Japan",
+    "List of investigational antidepressants","Listening to Prozac",
+    "Neurobiological effects of physical exercise","Pharmacology of antidepressants",
+    "Second-generation antidepressant","Serotonin antagonist and reuptake inhibitor",
+     "Serotonin modulator and stimulator"]
 # %%
 def remove_from_links(category, to_remove):
     for i in to_remove:
@@ -126,36 +126,36 @@ def remove_from_links(category, to_remove):
             print(f"Unable to remove{i}")
 
 # %% 
-# remove_from_links(ad_tree, to_remove)
-# ad_tree["sub-categories"]["Norepinephrine-dopamine reuptake inhibitors"]["links"].remove("List of methylphenidate analogues")
-# to_remove= ["Antidepressant discontinuation syndrome","Allosteric serotonin reuptake inhibitor",
-#             "Development and discovery of SSRI drugs", "Selective serotonin reuptake inhibitor"]
-# remove_from_links(ad_tree["sub-categories"]["Selective serotonin reuptake inhibitors"], to_remove)
-# to_remove= ["Antidepressant discontinuation syndrome","Serotonin–norepinephrine reuptake inhibitor"]
-# remove_from_links(ad_tree["sub-categories"]["Serotonin-norepinephrine reuptake inhibitors"], to_remove)
-# psychoanaleptics_subs["Antidementia agents"]["links"].remove("Template:Anti-dementia drugs")
-# # Remove the top-level antidepressants category (duplicate)
-# del root_subs["Antidepressants"]
-# ### Remove psychedelics, euphoriants, deliriants and dissociative drugs, which aren;t nootropics
-# del root_subs["Deliriants"]
-# del root_subs["Psychedelic drugs"]
-# del root_subs["Dissociative drugs"]
-# del root_subs["Euphoriants"]
-# # Psycholeptics are also likely nonrelevant
-# del root_subs["Psycholeptics"]
+remove_from_links(ad_tree, to_remove)
+ad_tree["sub-categories"]["Norepinephrine-dopamine reuptake inhibitors"]["links"].remove("List of methylphenidate analogues")
+to_remove= ["Antidepressant discontinuation syndrome","Allosteric serotonin reuptake inhibitor",
+            "Development and discovery of SSRI drugs", "Selective serotonin reuptake inhibitor"]
+remove_from_links(ad_tree["sub-categories"]["Selective serotonin reuptake inhibitors"], to_remove)
+to_remove= ["Antidepressant discontinuation syndrome","Serotonin–norepinephrine reuptake inhibitor"]
+remove_from_links(ad_tree["sub-categories"]["Serotonin-norepinephrine reuptake inhibitors"], to_remove)
+psychoanaleptics_subs["Antidementia agents"]["links"].remove("Template:Anti-dementia drugs")
+# Remove the top-level antidepressants category (duplicate)
+del root_subs["Antidepressants"]
+### Remove psychedelics, euphoriants, deliriants and dissociative drugs, which aren;t nootropics
+del root_subs["Deliriants"]
+del root_subs["Psychedelic drugs"]
+del root_subs["Dissociative drugs"]
+del root_subs["Euphoriants"]
+# Psycholeptics are also likely nonrelevant
+del root_subs["Psycholeptics"]
 
-# # %% [markdown]
-# # ### Anxiolitics
+# %% [markdown]
+# ### Anxiolitics
 
-# # %%
-# anxiolitics = root_subs["Anxiolytics"]
-# to_delete = ["List of investigational anxiolytics", "Serotonin antagonist and reuptake inhibitor"]
-# remove_from_links(anxiolitics, to_delete)
-# barbiturates = anxiolitics["sub-categories"]["Barbiturates"]
-# del barbiturates["sub-categories"]["Barbiturates-related deaths"]
-# to_delete = ["Mood stabilizer", "List of antidepressants","List of adverse effects of valproate semisodium",
-#             "List of investigational antidepressants","Template:Mood stabilizers"]
-# remove_from_links(root_subs["Mood stabilizers"], to_delete)
+# %%
+anxiolitics = root_subs["Anxiolytics"]
+to_delete = ["List of investigational anxiolytics", "Serotonin antagonist and reuptake inhibitor"]
+remove_from_links(anxiolitics, to_delete)
+barbiturates = anxiolitics["sub-categories"]["Barbiturates"]
+del barbiturates["sub-categories"]["Barbiturates-related deaths"]
+to_delete = ["Mood stabilizer", "List of antidepressants","List of adverse effects of valproate semisodium",
+            "List of investigational antidepressants","Template:Mood stabilizers"]
+remove_from_links(root_subs["Mood stabilizers"], to_delete)
 
 # %% [markdown]
 # ### Save the resulting clean tree:
