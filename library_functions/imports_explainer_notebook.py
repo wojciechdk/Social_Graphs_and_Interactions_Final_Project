e77361c196d92610f6d9c66bda3a82730250e8f6
Wojciech Mazurkiewicz <wojciechdk@gmail.com>
2020-12-07 12:49:47 +0100

    Explainer Notebook: Added Basic degree stats. Created a more compact version of the overview of the most central nodes

--- library_functions/imports_explainer_notebook.py
+++ library_functions/imports_explainer_notebook.py
@@ -4,6 +4,10 @@ import pandas as pd
 import warnings
 import wojciech as w
 
+from IPython.core.interactiveshell import InteractiveShell
+from IPython.display import Markdown, display
+
+
 try:
     import library_functions as lf
 except ModuleNotFoundError:
@@ -15,3 +19,14 @@ except ModuleNotFoundError:
     from project.library_functions.config import Config
 
 warnings.filterwarnings('ignore')
+
+
+def printmd(string):
+    display(Markdown(string))
+
+
+# Maximum of rows to show for the pandas tables
+pd.options.display.max_rows = 100
+
+# Show all code outputs in the cell outputs
+InteractiveShell.ast_node_interactivity = "all"
